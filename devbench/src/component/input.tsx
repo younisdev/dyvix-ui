645edae585bf00a1a4800e796634c99c258667f8
younisdev <younisayoub10@gmail.com>
2026-08-13 16:35:34 +0300

    refactor(devbench): port DyvixInput to TypeScript

--- devbench/src/component/input.tsx
+++ devbench/src/component/input.tsx
@@ -1,11 +1,10 @@
 import { DyvixInput } from '../../../src';
-import { DYVIX_GLOBAL_ANIMATION, DYVIX_GLOBAL_THEME } from '../../../src';
 export function InputTest() {
   return (
     <>
       <DyvixInput
-        animation={DYVIX_GLOBAL_ANIMATION.AURORA}
-        theme={DYVIX_GLOBAL_THEME.SUNSET}
+        animation={"drift"}
+        theme={"Crimson"}
         type="text"
         placeholder={'Enter your name'}
       />
