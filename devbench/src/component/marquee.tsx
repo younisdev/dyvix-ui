f343ebb43ec7100054a306a008b4355ea36d6017
younisdev <younisayoub10@gmail.com>
2026-08-30 23:25:16 +0300

    feat(marquee): add initial infinite DyvixMarquee foundation

---
+++ devbench/src/component/marquee.tsx
@@ -0,0 +1,12 @@
+import { DyvixMarquee, DyvixMarqueeItem } from '../../../src';
+
+export function MarqueeTest() {
+  return (
+    <DyvixMarquee>
+      <DyvixMarqueeItem>hi</DyvixMarqueeItem>
+      <DyvixMarqueeItem>hei</DyvixMarqueeItem>
+      <DyvixMarqueeItem>hi2</DyvixMarqueeItem>
+      <DyvixMarqueeItem>hi11</DyvixMarqueeItem>
+    </DyvixMarquee>
+  );
+}
